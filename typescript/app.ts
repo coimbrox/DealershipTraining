1607449e2acc6b3bb4749f939e245d346db53cc9
Gabriel Coimbra <gabao.coimbra@gmail.com>
2020-11-27 01:34:23 -0300

    Inferencia de tipos

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,5 +1,6 @@
-let hello = (nome: string, lastname: string) => {
-  console.log("Olá" + nome + lastname)
-}
+let mensagem: string = "Seja bem Vindo!"
+let TemporadaSPN: number = 15
+let estudandoAngular: boolean = true
+let listaDeFrutas: Array<string> = ['Maçã', 'Manga', 'Uva']
+let notaDasProvas: Array<number> = [9.0, 10.0, 7.0]
 
-hello("Gabriel")  
--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -1,5 +1,5 @@
-"use strict";
-var hello = function (nome, lastname) {
-    console.log("Olá" + nome + lastname);
-};
-hello("Gabriel");
+let mensagem = "Seja bem Vindo!"
+let TemporadaSPN = 15
+let estudandoAngular = true
+let listaDeFrutas = ['Maçã', 'Manga', 'Uva']
+let notaDasProvas = [9.0, 10.0, 7.0]
