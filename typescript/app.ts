36d44bc96cab7fd1c1ec08505cb916d97bf471b7
Gabriel Coimbra <gabao.coimbra@gmail.com>
2020-11-27 01:38:08 -0300

    Inferencia de tipos

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,6 +1,12 @@
 let mensagem: string = "Seja bem Vindo!"
 let TemporadaSPN: number = 15
 let estudandoAngular: boolean = true
+
 let listaDeFrutas: Array<string> = ['Maçã', 'Manga', 'Uva']
+let listaDeFrutas2: string[] = ['Maçã', 'Manga', 'Uva']
+
+
+
 let notaDasProvas: Array<number> = [9.0, 10.0, 7.0]
+let notaDasProvas2: number[] = [9.0, 10.0, 7.0]
 
